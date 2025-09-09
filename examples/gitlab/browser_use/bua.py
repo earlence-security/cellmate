9d04c37b1b7a8ff205379a8a6ce3a9fa07f7d63b
Luoxi Meng <lumeng@ucsd.edu>
2025-09-09 08:58:24 +0800

    [Examples] Update gitlab/browser_use example (#1)

--- examples/gitlab/browser_use/bua.py
+++ examples/gitlab/browser_use/bua.py
@@ -3,7 +3,6 @@ import requests
 from browser_use import Agent
 from browser_use.browser import BrowserSession
 from browser_use.llm import ChatAnthropic
-from cellmate.interface import Cellmate
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -12,36 +11,14 @@ task = """
 Go to https://gitlab.com/macaroon777sweet-group/macaroon777sweet-project/-/issues/2 and comment that "we are working on it".
 """
 
-# Define the paths and other parameters
-TASK = task
-STORAGE_PATH = "./local"
-DOMAIN_LIST_PATH = "./local/domains.json"
-RESOURCE_PATH = "./resource"
-
 def main():
-    # Instantiate the Cellmate class
-    cellmate = Cellmate(
-        task=TASK,
-        storage_dir_path=STORAGE_PATH,
-        domain_list_path=DOMAIN_LIST_PATH,
-        resource_dir_path=RESOURCE_PATH,
-        interface_mode='CLI'
-    )
-
-    # Run the cellmate instance
-    cellmate.run()
-
-    # Have a browser instance running with the policy enforcer extension active by this point of time
-    # Otherwise cellmate.run() will block until the extension has received the user defined policy
-
-    # Envoke the BUA here
-    # ENSURE that the BUA is operating on the browser instance with the extension active!
-
     asyncio.run(run_agent())
+
 
 def get_cdp_ws_url(port: int = 9222) -> str:
     resp = requests.get(f"http://localhost:{port}/json/version").json()
     return resp["webSocketDebuggerUrl"]
+
 
 async def run_agent():
     agent = Agent(
@@ -55,4 +32,5 @@ async def run_agent():
     )
     await agent.run(max_steps=20)
 
-main()
+if __name__ == "__main__":
+    main()
--- examples/gitlab/browser_use/bua_cellmate.py
+++ examples/gitlab/browser_use/bua_cellmate.py
@@ -3,6 +3,8 @@ import requests
 from browser_use import Agent
 from browser_use.browser import BrowserSession
 from browser_use.llm import ChatAnthropic
+import cellmate
+from cellmate.interface import Cellmate
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -11,12 +13,36 @@ task = """
 Go to https://gitlab.com/macaroon777sweet-group/macaroon777sweet-project/-/issues/2 and comment that "we are working on it".
 """
 
+# Define the paths and other parameters
+TASK = task
+STORAGE_PATH = "../local"
+RESOURCE_PATH = "../resource"
+EXTENSION_PATH = "../../../cellmate/DNR_enforcer"
+
 def main():
+    # Instantiate Cellmate
+    cellmate = Cellmate(
+        storage_dir_path=STORAGE_PATH,
+        resource_dir_path=RESOURCE_PATH,
+        interface_mode='CLI'
+    )
+
+    # Run the cellmate instance
+    cellmate.policy_setup(task=TASK)
+    # Signal the browser extension to start enforcing current policies
+    cellmate.start_enforcement()
+
+    # Start Browser-Use agent
     asyncio.run(run_agent())
+
+    # Signal the browser extension to stop policy enforcement
+    cellmate.end_enforcement()
+
 
 def get_cdp_ws_url(port: int = 9222) -> str:
     resp = requests.get(f"http://localhost:{port}/json/version").json()
     return resp["webSocketDebuggerUrl"]
+
 
 async def run_agent():
     agent = Agent(
@@ -30,4 +56,5 @@ async def run_agent():
     )
     await agent.run(max_steps=20)
 
-main()
+if __name__ == "__main__":
+    main()
